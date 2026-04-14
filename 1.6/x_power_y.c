ed105950498bf6fbb95f1ecd8e3c97cdab4409e0
Jin <jinyk226@gmail.com>
2026-04-14 08:34:36 -0700

    add to x power y

--- 1.6/x_power_y.c
+++ 1.6/x_power_y.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 
-int power(int m, int n);
+int power_v1(int m, int n);
+int power_v2(int m, int n);
 
 int main()
 {
   int i;
 
   for (i = 0; i < 10; i++)
-    printf("%d %d %d\n", i, power(2, i), power(-3, i));
+    printf("%d %d %d\n", i, (2, i), (-3, i));
   return 0;
 }
 
-int power(int base, int n)
+int power_v1(int base, int n)
 {
   int i, p;
 
   p = 1;
   for (i = 1; i <= n; i++)
+    p = p * base;
+  return p;
+}
+
+int power_v2(int base, int n)
+{
+  int p;
+
+  for (p = 1; n > 0; n--)
     p = p * base;
   return p;
 }
